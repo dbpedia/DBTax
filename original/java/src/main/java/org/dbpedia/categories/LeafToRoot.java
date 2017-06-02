fedf9e5dc3b588ca40b6d20a75c3e975421b381f
sm86 <shashank.048@iiitb.org>
2017-06-02 14:19:42 +0530

    added missing libraries

--- original/java/src/main/java/org/dbpedia/categories/LeafToRoot.java
+++ original/java/src/main/java/org/dbpedia/categories/LeafToRoot.java
@@ -3,6 +3,19 @@ package org.dbpedia.categories;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.aksw.jena_sparql_api.core.QueryExecutionFactory;
 import org.aksw.jena_sparql_api.delay.core.QueryExecutionFactoryDelay;
 import org.aksw.jena_sparql_api.http.QueryExecutionFactoryHttp;
--- original/java/src/main/java/org/dbpedia/categories/WikidataLabelResolver.java
+++ original/java/src/main/java/org/dbpedia/categories/WikidataLabelResolver.java
@@ -1,5 +1,17 @@
 package org.dbpedia.categories;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Description
  *
